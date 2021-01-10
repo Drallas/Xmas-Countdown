3c12fe96bc3490d8dbb6bd8c38f947794b9cc5fa
Drallas <drallas@icloud.com>
2021-01-10 19:55:25 +0100

    Adding current date

--- script.js
+++ script.js
@@ -6,29 +6,31 @@ var countDownDate = new Date(currentYear + "-12-25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
-    // Get todays date and time
-    var now = new Date().getTime();
+  // Get todays date and time
+  var now = new Date().getTime();
 
-    // Find the distance between now an the count down date
-    var distance = countDownDate - now;
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    var weeks = Math.floor(distance / (1000 * 60 * 60 * 24) / 7);
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor(
-        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+  var weeks = Math.floor(distance / (1000 * 60 * 60 * 24) / 7);
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in an element with id="demo"
-    document.getElementById("counter").innerHTML =
-        days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    document.getElementById("weeks").innerHTML = "nog " + weeks + " weken!";
+  // Display the result in an element with id="weeks"
+  document.getElementById("counter").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  document.getElementById("weeks").innerHTML = "nog " + weeks + " weken!";
 
-    // If the count down is finished, write some text
-    if (distance == 0) {
-        clearInterval(x);
-        document.getElementById("counter").innerHTML = "Fijne Kerst";
-    }
+  // Display the current time in id="current_date"
+  var todayItis = new Date()
+  document.getElementById("current_date").innerHTML = "Vandaag is het : " +  (todayItis.toLocaleDateString());
+
+  // If the count down is finished, write some text
+  if (distance == 0) {
+    clearInterval(x);
+    document.getElementById("counter").innerHTML = "Fijne Kerst";
+  }
 }, 1000);
